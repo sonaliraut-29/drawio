82c78f24cca933a7bc6f9d707e805342f5d70594
sonaliraut-29 <neosoftsonaliraut@gmail.com>
2023-09-26 20:28:35 +0530

    home page html

--- src/components/common/footer/Footer.js
+++ src/components/common/footer/Footer.js
@@ -1,6 +1,11 @@
 import React from "react";
 const Footer = () => {
-  return <div data-testid="footer">Here is a footer</div>;
+  return <div data-testid="footer">Here is a footer
+  
+  
+  
+
+  </div>;
 };
 
 export default Footer;
