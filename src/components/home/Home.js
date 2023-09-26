184d69b810ef8aaa361a32017b79cff23fd90285
sonaliraut-29 <neosoftsonaliraut@gmail.com>
2023-09-26 20:04:42 +0530

    changes and aws app

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import api from "../../redux/services/api";
+import { LEAFLETS } from "../../redux/reduxConstants/EndPoints";
 
 const Home = () => {
+  useEffect(() => {
+    fetchLeafts();
+  }, []);
+
+  const baseUrl = process.env.REACT_APP_API_BASEURL;
+
+  const fetchLeafts = () => {
+    api(baseUrl)
+      .get(LEAFLETS + "?days_tolerance=-25&num_of_rows_required=10")
+      .then((res) => {
+        console.log(res);
+        if (res.data.success) {
+        }
+      })
+      .catch((e) => console.log(e));
+  };
   return <div data-testid="home-page">Home page</div>;
 };
 
