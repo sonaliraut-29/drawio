1dc73ed0979b612a2e5b9996703e1f5e273e44fd
sonaliraut-29 <neosoftsonaliraut@gmail.com>
2023-09-30 18:06:07 +0530

    adding lazy loading

--- src/components/searchDetails/SearchDetails.js
+++ src/components/searchDetails/SearchDetails.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 import * as images from "../constant/Assets";
@@ -12,6 +12,9 @@ const SearchDetails = ({ history }) => {
   const [limit, setLimit] = useState(20);
   const [page, setPage] = useState(1);
   const [searchValue, setSearchValue] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [resultList, setResultList] = useState({ list: [] });
+  const loaderProfile = useRef(null);
 
   let fullSlug = "";
   let slugString = "";
@@ -37,14 +40,56 @@ const SearchDetails = ({ history }) => {
   }
 
   useEffect(() => {
+    const arrActualData = resultList && resultList.list;
+
+    const arrListData = productList;
+
+    const tempArr = [...resultList.list, ...arrListData];
+
+    setResultList({
+      list: tempArr,
+    });
+  }, [productList]);
+
+  useEffect(() => {
+    if (resultList && resultList.list.length > 0) {
+      var options = {
+        root: null,
+        rootMargin: "20px",
+        threshold: 1.0,
+      };
+
+      const observer = new IntersectionObserver(handleObserver, options);
+      if (loaderProfile.current) {
+        observer.observe(loaderProfile.current);
+      }
+    }
+  }, [resultList]);
+
+  const handleObserver = (entities) => {
+    const target = entities[0];
+    if (target.isIntersecting) {
+      setPage((page) => page + 1);
+    }
+  };
+
+  useEffect(() => {
     if (searchText && "" !== searchText) {
       fetchProductList(searchText);
     }
   }, [searchText]);
 
+  useEffect(() => {
+    page > 1 &&
+      fetchProductList(
+        searchValue && "" !== searchValue ? searchValue : searchText
+      );
+  }, [page]);
+
   const baseUrl = process.env.REACT_APP_API_BASEURL;
 
   const fetchProductList = (searchText) => {
+    setLoading(true);
     api(baseUrl)
       .get(
         SEARCH +
@@ -56,6 +101,7 @@ const SearchDetails = ({ history }) => {
           limit
       )
       .then((res) => {
+        setLoading(false);
         if (res.data.success) {
           setProductList(res.data.data);
         }
@@ -115,50 +161,54 @@ const SearchDetails = ({ history }) => {
             <div className="col-2 d-flex align-items-center">Filter</div>
           </Row>
         </section>
-        {productList && productList.length > 0 ? (
+        {resultList && resultList.list.length > 0 ? (
           <section className="search-products">
             <Row>
-              {productList.map((item, index) => {
-                let vendorName = item.Vendor.replace(" ", "-").toLowerCase();
-                return (
-                  <div
-                    className="col-6 col-sm-3 mb-4"
-                    onClick={() => handleLink(item.Item_URL)}
-                    style={{
-                      cursor: "pointer",
-                    }}
-                  >
-                    <div className="item">
-                      <div className="item-wrap">
-                        <img
-                          src={item.Item_Image_URL}
-                          alt="img"
-                          className="img-fluid"
-                        />
-                        <div className="item-desc">
+              {resultList &&
+                resultList.list.map((item, index) => {
+                  let vendorName = item.Vendor.replace(" ", "-").toLowerCase();
+                  return (
+                    <div
+                      className="col-6 col-sm-3 mb-4"
+                      onClick={() => handleLink(item.Item_URL)}
+                      style={{
+                        cursor: "pointer",
+                      }}
+                      key={"result" + index}
+                    >
+                      <div className="item">
+                        <div className="item-wrap">
                           <img
-                            src={
-                              item.vendor
-                                ? images[vendorName]
-                                : "./dist/assets/images/v2.png"
-                            }
+                            src={item.Item_Image_URL}
                             alt="img"
+                            className="img-fluid"
                           />
-                          <h5>{item.Brand}</h5>
-                          <p>{item.Item_name}</p>
-                        </div>
-                        <div className="price">
-                          <span>KD {item.Selling_Price} </span>
-                          <small>
-                            <strike>Old Price</strike>
-                          </small>
+                          <div className="item-desc">
+                            <img
+                              src={
+                                item.vendor
+                                  ? images[vendorName]
+                                  : "./dist/assets/images/v2.png"
+                              }
+                              alt="img"
+                            />
+                            <h5>{item.Brand}</h5>
+                            <p>{item.Item_name}</p>
+                          </div>
+                          <div className="price">
+                            <span>KD {item.Selling_Price} </span>
+                            <small>
+                              <strike>Old Price</strike>
+                            </small>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
             </Row>
+            <div className="loadingTemp" ref={loaderProfile} />
+            {loading && <InlineLoader />}
           </section>
         ) : (
           ""
@@ -168,3 +218,22 @@ const SearchDetails = ({ history }) => {
   );
 };
 export default SearchDetails;
+
+const InlineLoader = () => {
+  return (
+    <div id="floatingBarsG" style={{ marginBottom: "12px" }}>
+      <div className="blockG" id="rotateG_01"></div>
+      <div className="blockG" id="rotateG_02"></div>
+      <div className="blockG" id="rotateG_03"></div>
+      <div className="blockG" id="rotateG_04"></div>
+      <div className="blockG" id="rotateG_05"></div>
+      <div className="blockG" id="rotateG_06"></div>
+      <div className="blockG" id="rotateG_07"></div>
+      <div className="blockG" id="rotateG_08"></div>
+      <div className="blockG" id="rotateG_09"></div>
+      <div className="blockG" id="rotateG_10"></div>
+      <div className="blockG" id="rotateG_11"></div>
+      <div className="blockG" id="rotateG_12"></div>
+    </div>
+  );
+};
