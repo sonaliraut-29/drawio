4bc732d11645fa1d30ddc5e49dee6a71074272de
sonaliraut-29 <neosoftsonaliraut@gmail.com>
2023-10-10 19:33:45 +0530

    Adding change of banners

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 const Banner = () => {
-  return <div>Test Banner</div>;
+  return (
+    <div className="Banners">
+      <Container className="mt-5">
+        <section id="home__leaflet" className="mt-5 text-center">
+          <Row>
+            <Col className="d-flex justify-content-center align-items-center title-wrap mt-5 mb-4">
+              <h2 className="section-title mb-1">Banners</h2>
+              <span></span>
+            </Col>
+          </Row>
+
+          <Row></Row>
+        </section>
+      </Container>
+    </div>
+  );
 };
 
 export default Banner;
