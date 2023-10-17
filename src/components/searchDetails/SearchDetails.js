785788e6ce4fbaf0410022321905b2b982adc065
sonaliraut-29 <neosoftsonaliraut@gmail.com>
2023-10-17 17:59:28 +0530

    adidng changes

--- src/components/searchDetails/SearchDetails.js
+++ src/components/searchDetails/SearchDetails.js
@@ -1,25 +1,82 @@
 import React, { useState, useEffect } from "react";
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Row, Accordion } from "react-bootstrap";
 
 import * as images from "../constant/Assets";
 
 import api from "../../redux/services/api";
-import { SEARCH } from "../../redux/reduxConstants/EndPoints";
+import {
+  SEARCH,
+  CATEGORIES,
+  SUBCATEGORIES,
+} from "../../redux/reduxConstants/EndPoints";
 import Pagination from "../../uikit/Paginate";
 import CommunityLoaderCircularDash from "../../uikit/CommunityLoaderCircularDash";
+import "react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css";
+import RangeSlider from "react-bootstrap-range-slider";
 
 const SearchDetails = ({ history }) => {
+  const [value, setValue] = useState(0);
   const [productList, setProductList] = useState([]);
   const [limit, setLimit] = useState(20);
   const [page, setPage] = useState(1);
   const [searchValue, setSearchValue] = useState("");
   const [loading, setLoading] = useState(false);
 
+  const [categories, setCategories] = useState([]);
+  const [subCategories, setSubcategories] = useState([]);
+  const [actualSubcategories, setActualSubCategories] = useState([]);
+  const [actualCategories, setActualCategories] = useState([]);
+
+  useEffect(() => {
+    fetchCategories();
+    fetchSubcategories();
+  }, []);
+
+  useEffect(() => {
+    const subCategoriesTemp = [];
+    if (subCategories && subCategories.length > 0) {
+      subCategories.forEach((item) => {
+        const Category = item.Category;
+        subCategoriesTemp[Category] = subCategoriesTemp[Category] || [];
+        subCategoriesTemp[Category].push(item.Sub_Category);
+      });
+      const resultKeys = Object.keys(subCategoriesTemp);
+      const resultArray = Object.values(subCategoriesTemp);
+
+      setActualSubCategories(resultArray);
+      setActualCategories(resultKeys);
+    }
+  }, [subCategories]);
+
+  const baseUrl = process.env.REACT_APP_API_BASEURL;
+
+  const fetchCategories = () => {
+    api(baseUrl)
+      .get(CATEGORIES)
+      .then((res) => {
+        if (res.data.success) {
+          setCategories(res.data.data);
+        }
+      })
+      .catch((e) => console.log(e));
+  };
+
+  const fetchSubcategories = () => {
+    api(baseUrl)
+      .get(SUBCATEGORIES)
+      .then((res) => {
+        if (res.data.success) {
+          setSubcategories(res.data.data);
+        }
+      })
+      .catch((e) => console.log(e));
+  };
+
   let fullSlug = "";
   let slugString = "";
   let slug = "";
   let searchArr = "";
-  let searchText = ""; 
+  let searchText = "";
 
   if (window !== undefined && typeof window !== "undefined") {
     const paramArray = window.location.href.split("/");
@@ -43,8 +100,6 @@ const SearchDetails = ({ history }) => {
       fetchProductList(searchText);
     }
   }, [searchText]);
-
-  const baseUrl = process.env.REACT_APP_API_BASEURL;
 
   const fetchProductList = (searchText) => {
     setLoading(true);
@@ -148,67 +203,119 @@ const SearchDetails = ({ history }) => {
             <div className="col-2 d-flex align-items-center">Filter</div>
           </Row>
         </section>
-
-        {productList && productList.length > 0 ? (
-          <section className="search-products">
-            <Row>
-              {productList &&
-                productList.map((item, index) => {
-                  let vendorName = item.Vendor.replace(" ", "-").toLowerCase();
-                  return (
-                    <div
-                      className="col-6 col-sm-3 mb-4"
-                      onClick={() => handleLink(item.Item_URL)}
-                      style={{
-                        cursor: "pointer",
-                      }}
-                      key={"result" + index}
-                    >
-                      <div className="item">
-                        <div className="item-wrap">
-                          <img
-                            src={item.Item_Image_URL}
-                            alt="img"
-                            className="img-fluid"
-                          />
-                          <div className="item-desc">
-                            <img
-                              src={
-                                item.Vendor
-                                  ? images[vendorName]
-                                  : "./dist/assets/images/default-logo-sm.png"
-                              }
-                              alt="img"
-                            />
-                            <h5>{item.Brand}</h5>
-                            <p>{item.Item_name}</p>
-                          </div>
-                          <div className="price">
-                            <span>KD {item.Selling_Price} </span>
-                            <small>
-                              <strike>Old Price</strike>
-                            </small>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })}
-            </Row>
-            <Row className="">
-              <Col md={9} xs={12} className="pagination">
-                <Pagination
-                  totalCount={500}
-                  limitValue={limit}
-                  currentPage={page}
-                  handlePageClick={handlePageClick}
+        <section className="pt-0 pt-sm-5 pb-5">
+          <Row>
+            <div className="col-sm-3 cat-left">
+              <section className="cat-for-desktop">
+                {actualSubcategories && actualSubcategories.length > 0 ? (
+                  <Accordion defaultActiveKey={["0"]} alwaysOpen>
+                    {actualSubcategories.map((item, index) => {
+                      return (
+                        <Accordion.Item eventKey={index}>
+                          <Accordion.Header>
+                            {actualCategories[index]}
+                          </Accordion.Header>
+                          <Accordion.Body>
+                            <ul>
+                              {item.map((innerItem) => {
+                                return <li>{innerItem}</li>;
+                              })}
+                            </ul>
+                          </Accordion.Body>
+                        </Accordion.Item>
+                      );
+                    })}
+                  </Accordion>
+                ) : (
+                  ""
+                )}
+              </section>
+              <section>
+                <div>Price</div>
+                <RangeSlider
+                  value={value}
+                  onChange={(changeEvent) => setValue(changeEvent.target.value)}
+                  max="100000"
+                  min="0"
+                  tooltip="on"
                 />
-              </Col>
-            </Row>
-          </section>
-        ) : (
-          <p>Hang tight! Genie is searching high and low to find the best results for you. Sit back, relax, and let us do the work. Your wait will be worth it!</p>
-        )}
+                <div>0</div>
+                <div>100000</div>
+              </section>
+              <section className="cat-for-mobile"></section>
+            </div>
+
+            <div className="col-sm-9 cat-right">
+              {productList && productList.length > 0 ? (
+                <section className="cat-products">
+                  <Row>
+                    {productList &&
+                      productList.map((item, index) => {
+                        let vendorName = item.Vendor.replace(
+                          " ",
+                          "-"
+                        ).toLowerCase();
+                        return (
+                          <div
+                            className="col-6 col-sm-3 mb-4"
+                            onClick={() => handleLink(item.Item_URL)}
+                            style={{
+                              cursor: "pointer",
+                            }}
+                            key={"result" + index}
+                          >
+                            <div className="item">
+                              <div className="item-wrap">
+                                <img
+                                  src={item.Item_Image_URL}
+                                  alt="img"
+                                  className="img-fluid"
+                                />
+                                <div className="item-desc">
+                                  <img
+                                    src={
+                                      item.Vendor
+                                        ? images[vendorName]
+                                        : "./dist/assets/images/default-logo-sm.png"
+                                    }
+                                    alt="img"
+                                  />
+                                  <h5>{item.Brand}</h5>
+                                  <p>{item.Item_name}</p>
+                                </div>
+                                <div className="price">
+                                  <span>KD {item.Selling_Price} </span>
+                                  <small>
+                                    <strike>Old Price</strike>
+                                  </small>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        );
+                      })}
+                  </Row>
+                  <Row className="">
+                    <Col md={9} xs={12} className="pagination">
+                      <Pagination
+                        totalCount={500}
+                        limitValue={limit}
+                        currentPage={page}
+                        handlePageClick={handlePageClick}
+                      />
+                    </Col>
+                  </Row>
+                </section>
+              ) : (
+                <p>
+                  Hang tight! Genie is searching high and low to find the best
+                  results for you. Sit back, relax, and let us do the work. Your
+                  wait will be worth it!
+                </p>
+              )}
+            </div>
+          </Row>
+        </section>
       </Container>
     </main>
   );
